a2740e3a45cb05ed056b8e23cf7593afae02d3f6
eip <36315290+eip618@users.noreply.github.com>
2023-11-11 17:34:39 +1100

    the end of all things (4)
    the end of all things (3)
    
    the end of all things (2)
    
    the end of all things

--- nav.php
+++ nav.php
@@ -19,3 +19,6 @@
 
 </div>
 </div>
+<div class="dead">unfortunately, a lot of the information on here will be <b>outdated</b>. the most up to date and accurate information can be found at <a href="https://wiki.hacks.guide">the wiki</a> and <a href="https://hacks.guide">the guide</a>.
+<br>the information here will continue to be served for a) backup/redundancy and b) archival.</div>
+<br>
